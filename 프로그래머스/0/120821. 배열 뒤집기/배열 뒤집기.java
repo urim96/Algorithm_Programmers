7f675f50411f389340a17a18077d69263ccb6272
urim96 <157820470+urim96@users.noreply.github.com>
2024-01-29 14:29:35 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+        int a = num_list.length-1;
+            
+        for(int i = 0; i < num_list.length; i++){
+            answer[i] = num_list[a];
+            a--;
+        }  return answer;
+        
+        }
+        
+    }
