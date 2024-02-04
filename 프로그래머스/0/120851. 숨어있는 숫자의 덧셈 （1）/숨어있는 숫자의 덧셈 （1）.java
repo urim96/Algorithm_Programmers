6702e4cfd6064fd7e7fcb88ff0fd4604a77b9f58
urim96 <157820470+urim96@users.noreply.github.com>
2024-02-04 15:29:22 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.21 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        my_string = my_string.replaceAll("[a-zA-Z]","");
+        String[] str = my_string.split("");
+        for(String i : str){
+            answer += Integer.parseInt(i);
+        }
+        return answer;
+    }
+}
