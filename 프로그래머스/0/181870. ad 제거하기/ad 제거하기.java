3d3e6754a7d9b6df826bef401ffd995921123d78
urim96 <157820470+urim96@users.noreply.github.com>
2024-02-19 19:23:23 +0900

    [level 0] Title: ad 제거하기, Time: 0.79 ms, Memory: 82.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public  List<String> solution(String[] strArr) {
+        List<String> answer = new ArrayList<>();
+        for(int i = 0; i < strArr.length; i++){
+            if(strArr[i].contains("ad")==false){
+                answer.add(strArr[i]);
+            }
+        }
+        return answer;
+    }
+}
