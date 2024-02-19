51d795d00968066a943daaad563976e85dd4cba9
urim96 <157820470+urim96@users.noreply.github.com>
2024-02-19 20:50:02 +0900

    [level 0] Title: A 강조하기, Time: 0.03 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+
+        answer = myString.toLowerCase().replace("a","A");
+        return answer;
+    }
+}
