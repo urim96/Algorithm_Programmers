fbf6292abfecdb3f9312004aff6cc7d2457957e0
urim96 <157820470+urim96@users.noreply.github.com>
2024-02-19 19:46:53 +0900

    [level 0] Title: 문자열 밀기, Time: 1.33 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String A, String B) {
+        int answer = 0;
+
+         for(int i = 0; i < A.length(); i++){
+            if(A.equals(B)==true){
+            return answer;
+        }
+            String a = A.substring(A.length()-1);
+            A = a + A.substring(0, A.length()-1);
+            answer++;
+        }
+        return -1;
+    }
+}
