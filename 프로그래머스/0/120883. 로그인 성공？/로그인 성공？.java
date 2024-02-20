4b6eb90123ecbd728ffb053d12459ecdee582b71
urim96 <157820470+urim96@users.noreply.github.com>
2024-02-20 15:33:34 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        String answer = "";
+        for(int i = 0; i < db.length; i++){
+            if(id_pw[0].equals(db[i][0])==true&&id_pw[1].equals(db[i][1])==true){
+                answer = "login";
+            }else if(id_pw[0].equals(db[i][0])==true&&id_pw[1].equals(db[i][1])==false){
+                answer = "wrong pw";
+            }else if(id_pw[0].equals(db[i][0])==false&&id_pw[1].equals(db[i][1])==false) {answer = "fail";}
+        }
+        return answer;
+    }
+}
