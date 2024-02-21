28b6664ecb0159adf1a5874e006e21640f089009
urim96 <157820470+urim96@users.noreply.github.com>
2024-02-21 20:00:49 +0900

    [level 0] Title: 코드 처리하기, Time: 0.02 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+        int mode = 0;
+        for(int i = 0; i < code.length(); i++){
+            if(mode == 0){
+                if(code.charAt(i) == '1'){
+                   mode = 1;
+                    continue;
+                 } else if(i % 2 == 0){
+                    answer += code.charAt(i);
+                }   
+              }
+              if(mode == 1){
+                if(code.charAt(i) == '1'){
+                   mode = 0;
+                    continue;
+                 }else if(i % 2 == 1){
+                    answer += code.charAt(i);
+              }
+            }
+        } if(answer.equals("")){
+              answer = "EMPTY";
+      }
+        return answer;
+    }
+}
