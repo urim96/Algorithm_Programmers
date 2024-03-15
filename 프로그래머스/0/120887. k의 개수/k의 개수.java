b7f86c00d660de35380188c65c23946023f4ed30
urim96 <157820470+urim96@users.noreply.github.com>
2024-03-15 17:11:27 +0900

    [level 0] Title: k의 개수, Time: 63.40 ms, Memory: 104 MB -BaekjoonHub

---
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int i, int j, int k) {
+        int answer = 0;
+        String kk = Integer.toString(k);
+        
+        for(int a = i; a <= j; a++){
+            String b = Integer.toString(a);
+            String[] bb = b.split("");
+            for(int e = 0; e < bb.length; e++){
+                if(bb[e].contains(kk)){
+                 answer++;     
+                }
+               
+            }
+            
+        }
+        return answer;
+    }
+}
