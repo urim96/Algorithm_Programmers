48d8ef3e5db4f2abbd14dbdd6c30ad85431a7c03
urim96 <157820470+urim96@users.noreply.github.com>
2025-07-12 14:02:46 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 9.61 ms, Memory: 96.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        
+        for (int i = 0; i < s.length(); i++) {
+            boolean found = false;
+            for (int j = i - 1; j >= 0; j--) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    answer[i] = i - j;
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                answer[i] = -1;
+            }
+        }
+        return answer;
+    }
+}
